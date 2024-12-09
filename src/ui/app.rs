7318e9664e8199cef42075c92bca5341d4459195
Kenneth Price <krprice@ucsc.edu>
2024-12-08 16:02:40 -0800

    added polling for notifications every 2 minutes back in

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -30,8 +30,8 @@ pub struct App {
     pub image_manager: Arc<ImageManager>,
     post_update_sender: mpsc::Sender<PostView>,
     post_update_receiver: mpsc::Receiver<PostView>,
-    // notification_check_interval: Duration,
-    // last_notification_check: Instant,
+    notification_check_interval: Duration,
+    last_notification_check: Instant,
     update_manager: UpdateManager,
     pub post_composer: Option<PostComposer>,
     pub composing: bool,
@@ -50,8 +50,8 @@ impl App {
             image_manager,
             post_update_sender: sender,
             post_update_receiver: receiver,
-            // notification_check_interval: Duration::from_secs(120),
-            // last_notification_check: Instant::now(),
+            notification_check_interval: Duration::from_secs(120),
+            last_notification_check: Instant::now(),
             update_manager: UpdateManager::new(),
             post_composer: None,
             composing: false,
@@ -164,14 +164,14 @@ impl App {
         Ok(())
     }
 
-    // async fn check_notifications(&mut self) {
-    //     if self.last_notification_check.elapsed() >= self.notification_check_interval {
-    //         if let View::Notifications(notifications) = self.view_stack.current_view() {
-    //             notifications.load_notifications(&mut self.api).await.ok();
-    //         }
-    //         self.last_notification_check = Instant::now();
-    //     }
-    // }
+    async fn check_notifications(&mut self) {
+        if self.last_notification_check.elapsed() >= self.notification_check_interval {
+            if let View::Notifications(notifications) = self.view_stack.current_view() {
+                notifications.load_notifications(&mut self.api).await.ok();
+            }
+            self.last_notification_check = Instant::now();
+        }
+    }
 
     async fn handle_follow(&mut self) {
         let did = match self.view_stack.current_view() {
@@ -526,7 +526,7 @@ impl App {
             }
             
             if last_tick.elapsed() >= tick_rate {
-                // self.check_notifications().await;
+                self.check_notifications().await;
                 last_tick = Instant::now();
             }
         }
